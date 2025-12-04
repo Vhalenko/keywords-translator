b6220d55a8693380c6a8f833b7c3c63068e52129
Vhalenko <galenko.vladislav@gmail.com>
2025-12-04 09:44:10 +0100

    first commit

---
+++ js/countries.js
@@ -0,0 +1,10 @@
+const countries = {
+    "uk-UA": "Ukrainian",
+    "ar-SA": "Arabic",
+    "cs-CZ": "Czech",
+    "da-DK": "Danish",
+    "de-DE": "German",
+    "es-ES": "Spanish",
+    "nl-NL": "Dutch",
+    "it-IT": "Italian",
+}
---
+++ js/script.js
@@ -0,0 +1,83 @@
+const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector("#fromText");
+const toText = document.querySelector("#toText");
+const translateBtn = document.querySelector("#translateBtn");
+const languageContainer = document.querySelector("#language-container");
+const languageOption = document.querySelector(".target-language");
+const languageSelect = document.querySelector("#languageSelect");
+const errorMessage = document.querySelector(".error-message");
+let selectedLanguages = [];
+
+selectTag.forEach((tag) => {
+  for (const country_code in countries) {
+    let option = `<option class="target-language" value="${country_code}">${countries[country_code]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
+  }
+});
+
+languageSelect.addEventListener("change", () => {
+  const code = languageSelect.value;
+  const lang = countries[code];
+
+  // Prevent duplicates
+  const hasLanguage = Array.from(languageContainer.children).some(
+    (child) => child.dataset.code === code
+  );
+
+  if (hasLanguage) return;
+
+  selectedLanguages.push(code);
+
+  // Add the selected language as a tag
+  languageContainer.insertAdjacentHTML(
+    "beforeend",
+    `<p class="language" data-code="${code}">${lang}</p>`
+  );
+});
+
+translateBtn.addEventListener("click", async () => {
+  errorMessage.innerHTML = "Loading...";
+
+  const text = fromText.value.trim();
+  const keywords = text.split(", ");
+  const translationsByLanguage = {};
+
+  for (const code of selectedLanguages) {
+    const apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=en|${code}`;
+    const translatedSentence = await translate(apiUrl);
+    translationsByLanguage[code] = translatedSentence.split(", ");
+  }
+
+  console.log(translationsByLanguage);
+
+  const finalList = [];
+  for (let i = 0; i < keywords.length; i++) {
+    for (const code of selectedLanguages) {
+      finalList.push(translationsByLanguage[code][i]);
+    }
+  }
+
+  console.log(finalList);
+
+  const uniqueFinalList = finalList.filter(
+    (item, index) =>
+      finalList.findIndex((x) => x === item) ===
+      index
+  );
+
+  console.log(uniqueFinalList);
+  errorMessage.innerHTML = "";
+  toText.value = uniqueFinalList.join(", ");
+});
+
+async function translate(apiUrl) {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
+    const data = await response.json();
+    return data?.responseData?.translatedText || "[translation unavailable]";
+  } catch (err) {
+    errorMessage.innerHTML = "Translation error:", err;
+    return "[translation unavailable]";
+  }
+}
